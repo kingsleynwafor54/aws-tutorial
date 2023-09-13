e7801a5f12ac480c1a20af75d16847d5d4292595
unknown <kingsleynwafor54@gmail.com>
2023-09-13 03:11:33 +0100

    aws_with_python_s3_

--- tutorial_one/aws_with_python_s3.py
+++ tutorial_one/aws_with_python_s3.py
@@ -6,8 +6,8 @@ import pandas as pd
 # Initialize a session using the Aws credentials
 
 session=boto3.Session(
-    aws_access_key_id='AKIAWZQJO4WO6JCBURFY',
-    aws_secret_access_key='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',
+    aws_access_key_id='AKIAWZQJO4WOXT3S7FOQ',
+    aws_secret_access_key='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',
     region_name='us-east-1'
 )
 
@@ -15,8 +15,9 @@ session=boto3.Session(
 # Create an S3 Client
 s3=session.client('s3')
 
+
+# List all s3 
 response = s3.list_buckets()
-# List all s3 buckets
 def get_listof_buckets():
     print('Existing buckets:')
     for bucket in response['Buckets']:
@@ -24,62 +25,67 @@ def get_listof_buckets():
 
 
 #Create a bucket
-# bucket_name='s3-bucket-kingsley1'
-# s3.create_bucket(Bucket=bucket_name)
-# get_listof_buckets()
+
+def create_s3_bucket(bucket_name):
+    s3.create_bucket(Bucket=bucket_name)
+    get_listof_buckets()
 
 
 # # Let's upload a file to our bucket
 def upload_file_to_s3(filepath,bucket_name,key):
-    # file_path=r'C:\Users\USER\Downloads\football.jpeg'
-    # s3.upload_file(file_path,"s3-bucket-kingsley1","football.jpeg")
-    # s3.upload_file('sample_data.csv',"s3-bucket-kingsley","sample_data.csv")
     s3.upload_file(filepath,bucket_name,key) 
-# s3.upload_file('sample_data.csv',"s3-bucket-kingsley","sample_data.csv")
 
 
-
-# s3.download_file('s3-bucket-kingsley1', 'football.jpeg', 'football.jpeg')
-# s3.download_file('s3-bucket-kingsley', 'sample_data.csv', 'sample_data.csv')
+# downloading file from aws s3 bucket
+def downloading_files_s3(bucket_name, object_name, file_name):
+    s3.download_file(bucket_name, object_name, file_name)
 
 
 
 #Deleting files in a bucket
-# bucket_name = 's3-bucket-kingsley1'
-# file_key = 'sample_data.csv'
-
-# try:
-#     # Delete the file
-#     s3.delete_object(Bucket=bucket_name, Key=file_key)
-#     print(f"File {file_key} deleted successfully from {bucket_name}")
-    
-# except Exception as e:
-#     print(f"Error deleting file: {e}")
+def delete_file_s3(bucket_name,file_key):
+    try:
+        # Delete the file
+        s3.delete_object(Bucket=bucket_name, Key=file_key)
+        print(f"File {file_key} deleted successfully from {bucket_name}")
+        
+    except Exception as e:
+        print(f"Error deleting file: {e}")
 
 
 
 
 # CHECK ALL FILES IN A BUCKET
-# try:
-#     # List all objects in the bucket
-#     objects = s3.list_objects_v2(Bucket=bucket_name)
+def checking_all_files_from_aws_s3_bucket(bucket_name):
+    try:
+        # List all objects in the bucket
+        objects = s3.list_objects_v2(Bucket=bucket_name)
 
-#     # Check if objects were found
-#     if 'Contents' in objects:
-#         print(f"Files in {bucket_name}:")
+        # Check if objects were found
+        if 'Contents' in objects:
+            print(f"Files in {bucket_name}:")
 
-#         # Iterate through the list of objects and print their keys (file paths)
-#         for obj in objects['Contents']:
-#             print(obj['Key'])
-#     else:
-#         print(f"No objects found in {bucket_name}")
-# except Exception as e:
-#     print(f"Error listing objects: {e}")
+            # Iterate through the list of objects and print their keys (file paths)
+            for obj in objects['Contents']:
+                print(obj['Key'])
+        else:
+            print(f"No objects found in {bucket_name}")
+    except Exception as e:
+        print(f"Error listing objects: {e}")
 
 # Delete the bucket
+def delete_s3_bucket(bucket_name):
+    s3.delete_bucket(Bucket=bucket_name)
 
-# s3.delete_bucket(Bucket='s3-bucket-kingsley1')
 
-
-
+bucket_name='s3-bucket-kingsley1'
+bucket_name = 's3-bucket-kingsley1'
+file_key = 'sample_data.csv'
+get_listof_buckets()
+create_s3_bucket(bucket_name)
+upload_file_to_s3('sample_data.csv',bucket_name,"sample_data.csv")
+downloading_files_s3(bucket_name, 'sample_data.csv', "sample_data1.csv")
+delete_file_s3(bucket_name,file_key)
+checking_all_files_from_aws_s3_bucket(bucket_name)
+delete_s3_bucket(bucket_name)
 
